8ec7e6db5c1ad7738e500f77d11aa8a1fe1edae2
amul1900 <40581959+amul1900@users.noreply.github.com>
2020-01-09 11:36:48 -0800

    Add files via upload

---
+++ Assignment11.R
@@ -0,0 +1,27 @@
+install.packages("DBI")
+install.packages("RSQLite")
+
+library(DBI)
+setwd("C:/@UCB_L11") 
+
+
+# Download the SQLite database file I'm hosting on GitHub
+download.file("https://github.com/KristenKehrer/datamovesme-sqlcourse/raw/gh-pages/assets/sqlcourse.db",
+              
+              "./sqlcourse.db",
+              
+              mode = "wb")
+
+# Open a connection to the SQLite database
+con <- dbConnect(RSQLite::SQLite(), "./sqlcourse.db")
+
+## List all of the tables
+dbListTables(con)
+
+## read the salescall table
+dbReadTable(con, "salescall")
+
+# Run the following query:
+queryResult <- dbSendQuery(con, "select max(customer_id) from salescall")
+dbFetch(queryResult)
+
